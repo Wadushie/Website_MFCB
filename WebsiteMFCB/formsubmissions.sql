7308c9b5f0890ba515a331e6aaccde06e2de8ada
Rogelio López <61802253+Wadushie@users.noreply.github.com>
2025-03-10 00:27:57 -0300

    Add files via upload

---
+++ WebsiteMFCB/formsubmissions.sql
@@ -0,0 +1,8 @@
+CREATE TABLE form_submissions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    subject VARCHAR(255) NOT NULL,
+    message TEXT NOT NULL,
+    submitted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
